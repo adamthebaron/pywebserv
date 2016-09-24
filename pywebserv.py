15ef571cdc04f33e917a1fbc0f876c935a1bf1f4
adam <akessle1@butler.edu>
2016-09-24 14:08:21 -0400

    add main loop and socket

--- pywebserv.py
+++ pywebserv.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
 	main()
 
-def servinit(argv):
+def getopts(argv):
 	try:
 		opts, args = getopt.getopt(argv[1:], "p:s:")
 	except getopt.GetoptError:
@@ -17,3 +17,18 @@ def servinit(argv):
 		elif opt == '-r':
 			root = arg
 	return port, root
+
+def servinit(port, root):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind(('',port))
+	sock.listen(1)
+	print("socket initialized")
+	while True:
+		# do stuff
+	sock.shutdown(socket.SHUT_RDWR)
+	sock.close()
+
+def handlecode(code):
+	# handle HTTP codes here
+
+
