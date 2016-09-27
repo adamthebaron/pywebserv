7da289f9c5740b07a15416ee81689c3c397be882
adam <akessle1@butler.edu>
2016-09-27 19:44:20 -0400

    fix indentation

--- pywebserv.py
+++ pywebserv.py
@@ -32,42 +32,42 @@ def usage():
     print("default root is cwd")
 
 def getopts(argv):
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hp:s:")
-	except getopt.GetoptError:
-	    usage()
-	    sys.exit()
-	for opt, arg in opts:
-		if opt == '-p':
-			port = int(arg)
-		elif opt == '-r':
-			root = arg
-		elif opt == '-h':
-		    usage()
-		    sys.exit()
-	return port, root
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hp:s:")
+    except getopt.GetoptError:
+        usage()
+        sys.exit()
+    for opt, arg in opts:
+        if opt == '-p':
+            port = int(arg)
+        elif opt == '-r':
+            root = arg
+        elif opt == '-h':
+            usage()
+        sys.exit()
+    return port, root
 
 def servinit(port, root):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind(('',port))
-	print("socket initialized")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('',port))
+    print("socket initialized")
 
 #def parseheader(req):
-	# parse the header here
+    # parse the header here
 
 #def handlecode(code):
-	# handle HTTP codes here
+    # handle HTTP codes here
 	
 def main(argv):
     port, root = getopts(argv)
     sock = servinit(port, root)
     sock.listen(1)
     while True:
-	    conn, addr = sock.accept()
-	    message = conn.recv(2048)
-	    resp = handlecode(message.decode('UTF-8'))
-	    conn.send(bytes(resp, 'UTF-8'))
-	    conn.close()
+        conn, addr = sock.accept()
+        message = conn.recv(2048)
+        resp = handlecode(message.decode('UTF-8'))
+        conn.send(bytes(resp, 'UTF-8'))
+        conn.close()
     sock.shutdown(socket.SHUT_RDWR)
     sock.close()
     
